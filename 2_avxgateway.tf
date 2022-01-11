dc54152f1c26b218c364c6848304ff0eb3f4a17d
bayupw <bayupw@gmail.com>
2022-01-12 08:30:51 +1300

    updated avxgateway.tf

--- 2_avxgateway.tf
+++ 2_avxgateway.tf
@@ -10,15 +10,3 @@ resource "aviatrix_gateway" "avx_standalone_gw" {
   gw_size      = "n1-standard-1"
   subnet       = module.gcp_prod_vpc.subnets_ips[count.index]
 }
-
-/* 
-# Create Standalone Aviatrix Gateway in EU
-resource "aviatrix_gateway" "eu_prod_1" {
-  cloud_type   = 4
-  account_name = var.gcp_account_name
-  gw_name      = "euprodgw"
-  vpc_id       = module.gcp_prod_vpc.network_name
-  vpc_reg      = "${module.gcp_prod_vpc.subnets_regions[1]}-b"
-  gw_size      = "n1-standard-1"
-  subnet       = module.gcp_prod_vpc.subnets_ips[1]
-} */
